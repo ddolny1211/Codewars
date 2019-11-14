e918794a4c025cd00c23a28dc873ad9839dbcb2d
Dawid <dawidddolny95@gmail.com>
2019-11-14 17:28:24 +0100

    Implement getX0 method

--- src/X0.java
+++ src/X0.java
@@ -1,2 +1,27 @@
-public class X0 {
+class XO {
+
+    //Check if a given String has the same amount of 'x' and 'o'
+    //Return type: boolean
+    public static boolean getXO(String str) {
+        int xCounter = 0;
+        int oCounter = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'x' || str.charAt(i) == 'X') {
+                xCounter++;
+            } else if (str.charAt(i) == 'o' || str.charAt(i) == 'O') {
+                oCounter++;
+            }
+        }
+        if (xCounter == oCounter) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "xxXooo";
+        System.out.println(getXO(str));
+    }
 }
