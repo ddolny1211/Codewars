31880bb9b6c8fb25286e2c770d43c24e33fef4a3
Dawid <dawidddolny95@gmail.com>
2019-11-14 18:12:48 +0100

    Implement findNb method

---
+++ src/ASum.java
@@ -0,0 +1,24 @@
+public class ASum {
+
+    //By given volume of the building (m) returns number of cubes need for build
+
+    public static long findNb(long m) {
+        long n = 1;
+        long currentVolume = m;
+        while (m > 0) {
+            currentVolume = n * n * n;
+            m -= currentVolume;
+            n++;
+        }
+        if (m == 0) {
+            return n - 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Long m = 40539911473216L;
+        System.out.println(findNb(m));
+    }
+}
