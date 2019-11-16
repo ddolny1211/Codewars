30feccb522ab5961554a33ed0b24260c649d1aa4
Dawid <dawidddolny95@gmail.com>
2019-11-16 21:47:04 +0100

    Add CamelCase solution

---
+++ src/six_kuy/CamelCase.java
@@ -0,0 +1,26 @@
+package six_kuy;
+
+public class CamelCase {
+    static String toCamelCase(String s) {
+        String[] splittedString = s.split("[-_]");
+        StringBuilder sb = new StringBuilder();
+        boolean wasFirstWord = false;
+        for (String word : splittedString) {
+            if (wasFirstWord) {
+                String wordUpperCase = word.substring(0, 1).toUpperCase() + word.substring(1);
+                sb.append(wordUpperCase);
+            } else {
+                sb.append(word);
+                wasFirstWord = true;
+            }
+        }
+        System.out.println(sb.toString());
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        CamelCase.toCamelCase("the-stealth-warrior");
+        CamelCase.toCamelCase("The_Stealth_Warrior");
+    }
+}
