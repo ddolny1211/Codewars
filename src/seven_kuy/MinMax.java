2da6129a95c045d461ba22a1c928084eb1c62d8c
Dawid <dawidddolny95@gmail.com>
2019-11-16 21:28:51 +0100

    Add MinMax solution

---
+++ src/seven_kuy/MinMax.java
@@ -0,0 +1,24 @@
+package seven_kuy;
+
+public class MinMax {
+    public static int[] minMax(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            } else if (num < min) {
+                min = num;
+            }
+        }
+        return new int[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] newArray = MinMax.minMax(array);
+        for (int num : newArray) {
+            System.out.println(num);
+        }
+    }
+}
