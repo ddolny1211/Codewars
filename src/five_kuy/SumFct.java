71e2f4c26de1478c422f395be6e1a1d48ba7bc5a
Dawid <dawidddolny95@gmail.com>
2019-11-16 23:41:56 +0100

    Add SumFct class and solution

---
+++ src/five_kuy/SumFct.java
@@ -0,0 +1,27 @@
+package five_kuy;
+
+import java.math.BigInteger;
+
+public class SumFct {
+    public static BigInteger perimeter(BigInteger n) {
+        BigInteger first = BigInteger.ZERO;
+        BigInteger second = BigInteger.ONE;
+        BigInteger current;
+        BigInteger sum = BigInteger.ONE;
+        int iterator = 0;
+        while (iterator < n.intValue()) {
+            current = first.add(second);
+            first = second;
+            second = current;
+            sum = sum.add(current);
+            iterator++;
+        }
+        sum = sum.multiply(new BigInteger("4"));
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        SumFct.perimeter(new BigInteger("7"));
+
+    }
+}
